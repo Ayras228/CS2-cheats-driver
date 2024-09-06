b95dd80babec6de5fbc3e75fbaa53a60d3792a5a
Maksym <80080035@ukr.net>
2024-09-06 15:24:28 +0300

    1.1
    work version

--- um/src/main.cpp
+++ um/src/main.cpp
@@ -35,29 +35,13 @@ int main()
         {
             std::cout << "Client found.\n";
 
-            // Запуск bunny_hop в окремому потоці
+
             std::thread bunny_hop_thread(bunny_hop, std::ref(driver_manager), std::ref(client));
 
-           
-
-            //aim_bot(driver_manager, client);
-            // Запуск aim_bot в окремому потоці
-            //std::thread aim_bot_thread(aim_bot, std::ref(driver_manager), std::ref(client));
-
-            // Основний потік може виконувати інші завдання або чекати завершення додатку
-            //aim_bot_thread.join(); // Приєднання потоку до основного (опціонально)
-            // Основний потік може виконувати інші завдання або чекати завершення додатку
-            //bunny_hop_thread.join(); // Приєднання потоку до основного (опціонально)
-
-            //aim_bot_(driver_manager, client);
-             //Запуск aim_bot в окремому потоці
             std::thread aim_bot_thread(aim_bot, std::ref(driver_manager), std::ref(client));
 
-             //Основний потік може виконувати інші завдання або чекати завершення додатку
-            aim_bot_thread.join(); // Приєднання потоку до основного (опціонально)
-
-            //Основний потік може виконувати інші завдання або чекати завершення додатку
-            bunny_hop_thread.join(); // Приєднання потоку до основного (опціонально)
+            aim_bot_thread.join();
+            bunny_hop_thread.join();
 
         }
     }
